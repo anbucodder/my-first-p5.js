6bafdfd1fda7d5d816d3b3de127bb0fd2cf6c429
anbucodder <100592337+anbucodder@users.noreply.github.com>
2023-06-13 18:16:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,27 @@
 function preload(){
-
 }
-
 function setup(){
-    canvas = createCanvas(650, 500);
-    canvas.position(110,250);
+    canvas = createCanvas(600,550);
+    canvas.center();
+
     video = createCapture(VIDEO);
-    video.
     video.hide();
-
-    tint_color="";
 }
-
-function draw(){
-    image(video,0,0,400,350);
-    fill(255,0,100);
-    stroke(255,0,100)
-    circle(50,50,100)
-    
-}
-
 function take_snapshot(){
     save("anbu.png");
 }
-
-function filter_tint(){
-    if(document.getElementById("color_name").value==""){
-        document.getElementById("color").innerHTML="Please fill the Textinput"
-        document.getElementById("color").style.color=red;
-    }
-    else{
-    tint_color=document.getElementById("color_name").value
-    }
+function draw(){
+    image(video,50,50,500,450);
+    fill(0,200,50);
+    stroke(0,200,50);
+    rect(50, 50, 20, 450);
+    rect(50,50,490,20);
+    rect(530,60,20,450);
+    rect(60,480,490,20);
+    stroke(255,0,0);
+    fill(255,0,0);
+    circle(60, 60, 50);
+    circle(540,60,50);
+    circle(60,490,50);
+    circle(540, 490, 50);
 }
